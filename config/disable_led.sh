5a696ee94a35436f7738b348d46dd9a1770fb096
zlServiceZ <Julian-Richter.98@web.de>
2024-01-09 12:44:18 +0100

    Added pihole Script

--- config/disable_led.sh
+++ config/disable_led.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "Disable LEDs"
+
 text_to_append="# Turn off the LEDs
 dtparam=pwr_led_trigger=default-on # The default
 dtparam=pwr_led_activelow=off
@@ -7,6 +9,8 @@ dtparam=act_led_trigger=default-on # The default
 dtparam=act_led_activelow=off"
 
 echo "$text_to_append" | sudo tee -a /boot/config.txt > /dev/null
+
+echo "Disabled LEDs"
 
 # reboot is necessary
 source "$HOME/rpi/tools/restart.sh"
