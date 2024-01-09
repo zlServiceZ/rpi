988c4ab21da06705be150e01f95c292c325a3f47
zlServiceZ <Julian-Richter.98@web.de>
2024-01-09 12:38:54 +0100

    Added pihole Script

---
+++ config/disable_led.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+text_to_append="# Turn off the LEDs
+dtparam=pwr_led_trigger=default-on # The default
+dtparam=pwr_led_activelow=off
+dtparam=act_led_trigger=default-on # The default
+dtparam=act_led_activelow=off"
+
+echo "$text_to_append" | sudo tee -a /boot/config.txt > /dev/null
--- pihole/install_pihole.sh
+++ pihole/install_pihole.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
+sudo apt update
+sudo apt upgrade -y
+
 curl -sSL https://install.pi-hole.net | bash
+
+# change password
+pihole -a -p
