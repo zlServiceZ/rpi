cec62017edaa8f187b7a439f54c2940d4fa3e6c1
zlServiceZ <Julian-Richter.98@web.de>
2024-02-13 09:32:48 +0100

    Added home_assistant Script

---
+++ home_assistant/install_home_assistant1.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y
+sudo apt install \
+apparmor \
+cifs-utils \
+curl \
+dbus \
+jq \
+libglib2.0-bin \
+lsb-release \
+network-manager \
+nfs-common \
+systemd-journal-remote \
+systemd-resolved \
+udisks2 \
+wget -y
+
+curl -fsSL get.docker.com | sh
+
+sudo groupadd docker
+sudo usermod -aG docker $USER
+
+sudo bash -c 'cat << EOF >> /etc/default/grub
+systemd.unified_cgroup_hierarchy=false
+EOF'
+
+sudo bash -c 'cat << EOF >> /boot/firmware/cmdline.txt
+ apparmor=1 security=apparmor
+EOF'
--- home_assistant/install_home_assistant2.sh
+++ home_assistant/install_home_assistant2.sh
@@ -1,33 +1,5 @@
 #!/bin/bash
 
-sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y
-sudo apt install \
-apparmor \
-cifs-utils \
-curl \
-dbus \
-jq \
-libglib2.0-bin \
-lsb-release \
-network-manager \
-nfs-common \
-systemd-journal-remote \
-systemd-resolved \
-udisks2 \
-wget -y
-
-curl -fsSL get.docker.com | sh
-
-sudo groupadd docker
-sudo usermod -aG docker $USER
-
-sudo bash -c 'cat << EOF >> /etc/default/grub
-systemd.unified_cgroup_hierarchy=false
-EOF'
-
-sudo bash -c 'cat << EOF >> /boot/firmware/cmdline.txt
- apparmor=1 security=apparmor
-EOF'
 
 # install os agent
 wget https://github.com/home-assistant/os-agent/releases/download/1.6.0/os-agent_1.6.0_linux_aarch64.deb
@@ -35,8 +7,8 @@ sudo dpkg -i os-agent_1.6.0_linux_aarch64.deb
 sudo rm -r os-agent_1.6.0_linux_aarch64.deb
 gdbus introspect --system --dest io.hass.os --object-path /io/hass/os
 
-systemctl start systemd-journal-gatewayd.service
-systemctl start systemd-journal-gatewayd.socket
+sudo systemctl start systemd-journal-gatewayd.service
+sudo systemctl start systemd-journal-gatewayd.socket
 
 # install home assistant supervised
 wget -O homeassistant-supervised.deb https://github.com/home-assistant/supervised-installer/releases/latest/download/homeassistant-supervised.deb
